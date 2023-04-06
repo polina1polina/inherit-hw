af3a84a5692fd255ac6289e3d7a4607d52dd7add
polina <polina.matyusha@gmail.com>
2023-04-07 00:29:58 +0300

    commit

---
+++ src/main/java/Meeting.java
@@ -0,0 +1,36 @@
+public class Meeting extends Task {
+
+    private final String topic;
+    private String project;
+    private final String start;
+
+    public Meeting(int id, String topic, String start, String воВторникПослеОбеда) {
+        super(id);
+        this.project = project;
+        this.start = start;
+        this.topic = topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if (topic.contains(query)) {
+            return true;
+        }
+        if (project.contains(query)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void matches() {
+    }
+}
